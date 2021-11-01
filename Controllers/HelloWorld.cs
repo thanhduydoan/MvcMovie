1c8360da320957bfdc0ede79ca7368fae89d4a1f
DESKTOP-RAAIMNI\ACER <exanp>
2021-11-01 17:12:03 +0700

    Done

---
+++ Controllers/HelloWorld.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
